30334e4e1e30654361af9a7ee5a87fd65f3dbed8
Elias Correa <85316737+EliasCorrea@users.noreply.github.com>
2026-02-06 19:29:49 -0300

    coagulacion

--- AGENTS.md
+++ AGENTS.md
@@ -2,7 +2,7 @@
 - Project name: medical-sim-base_VR
 - Unity version: Unity 6000.3.2f1
 - Active game object:
-  - Name: DrillTip
+  - Name: Teleport Interactor
   - Tag: Untagged
   - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ Assets/Scripts/HemostasiaTip.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class HemostasiaTip : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag(""))
+        {
+
+        }
+    }
+}
